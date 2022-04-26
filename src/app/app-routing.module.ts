c19961229055eb646fec0ad353468a803e9354e2
chentitorupa <ruizvicente020@gmail.com>
2022-04-26 00:22:42 -0500

    Se crearon rutas y se creó componente error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full'},
+  { path: 'list-empleados', component: ListEmpleadosComponent},
+  { path: 'create-empleado', component: CreateEmpleadoComponent},
+  { path: '404', component:ErrorComponent},
+  { path: '**', redirectTo:'404', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmpleadosComponent,
     CreateEmpleadoComponent,
-    NavbarComponent
+    NavbarComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
