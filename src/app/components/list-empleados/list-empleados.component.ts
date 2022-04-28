c6ed39d1354a9f8f2caa30bf89264fe8faa9df8f
chentitorupa <ruizvicente020@gmail.com>
2022-04-28 01:17:37 -0500

    Se agrego el listado desde firestore a la tabla de datos. También se añadió que se puedan eliminar empleados dando clic al ícono 'trash'

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-list-empleados',
@@ -9,11 +11,44 @@ import { Observable } from 'rxjs';
 })
 export class ListEmpleadosComponent implements OnInit {
 
-  constructor() {
+  employees: any[] = []
+
+  constructor(private _empleadoService: EmpleadoService,
+              private toastr: ToastrService) {
     
   }
 
   ngOnInit(): void {
+    this.getEmployees()
+  }
+
+  // Listar empleados
+  getEmployees(){
+    this._empleadoService.getEmployees().subscribe( data => {
+      
+      this.employees = [];
+
+      data.forEach((element:any) => {
+        /*console.log(element.payload.doc.id)*/
+        //console.log(element.payload.doc.data())
+        this.employees.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.employees);
+    })
+  }
+
+  deleteEmployee(id: string){
+    this._empleadoService.deleteEmployee(id).then( () =>{
+       //console.log("Empleado eliminado con exito");
+       this.toastr.error('Empleado eliminado con exito!', 'Empleado eliminado', {
+        positionClass: 'toast-bottom-right'
+      });
+    }).catch(error => {
+      console.log(error);
+    })
   }
 
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,14 @@ export class EmpleadoService {
    addEmployee(employee: any): Promise<any>{
       return this.firestore.collection('employees').add(employee)
    }
+
+   // Listar empleados 
+   getEmployees(): Observable<any>{
+     return this.firestore.collection('employees', ref => ref.orderBy('createDate', 'desc')).snapshotChanges();
+   }
+
+   // Eliminar empleado por id
+  deleteEmployee(id: string): Promise<any>{
+    return this.firestore.collection('employees').doc(id).delete();
+  }
 }
